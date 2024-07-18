e9cf108d5b052923fa024329edf01ca8d41e0964
Youssef-001 <youssefsamir432@gmail.com>
2024-07-18 16:08:31 +0300

    Fix UpdateBoard function, added styles for win/tie

--- app.js
+++ app.js
@@ -9,8 +9,15 @@ function Game() {
             gameBoard[i] = [];
             for (let j = 0; j < 3; j++) {
                 gameBoard[i][j] = 'S';
+                let cell = document.getElementById(`${i} ${j}`);
+                cell.innerHTML = '';
+                cell.style.backgroundColor = ''
             }
-        }}
+        }
+    
+    
+    
+    }
 
         function addPlayer(name)
         {
@@ -30,7 +37,12 @@ function Game() {
         {
             if (gameBoard[i][0] === gameBoard[i][1] && gameBoard[i][1] === gameBoard[i][2] && gameBoard[i][0] === gameBoard[i][2] && gameBoard[i][0] !== 'S')
             {
-                return gameBoard[i][0];
+                // return gameBoard[i][0];
+                return {winner: gameBoard[i][0],
+                    c1: `${i} 0`,
+                    c2: `${i} 1`,
+                    c3: `${i} 2`,
+                }
             }
 
         }
@@ -39,17 +51,33 @@ function Game() {
             {
                 if (gameBoard[0][i] === gameBoard[1][i] && gameBoard[1][i] === gameBoard[2][i] && gameBoard[0][i] === gameBoard[2][i] && gameBoard[0][i] !== 'S')
                 {
-                    return gameBoard[0][i];
+                    return {winner: gameBoard[i][0],
+                        c1: `0 ${i}`,
+                        c2: `1 ${i}`,
+                        c3: `2 ${i}`,
+                    }
                 }
                 
             }
 
         if (gameBoard[0][0] == gameBoard[1][1] && gameBoard[1][1] == gameBoard[2][2] && gameBoard[0][0] == gameBoard[2][2] && gameBoard[0][0] != 'S')
-            return gameBoard[0][0];
+           {
+            return {winner: gameBoard[0][0],
+                c1: `0 0`,
+                c2: `1 1`,
+                c3: `2 2`,
+            }
+           }
         
 
         if (gameBoard[0][2] === gameBoard[1][1] && gameBoard[1][1] === gameBoard[2][0] && gameBoard[0][2] === gameBoard[2][0] && gameBoard[0][2] != 'S')
-            return gameBoard[0][2];
+        {
+            return {winner: gameBoard[0][2],
+                c1: `0 2`,
+                c2: `1 1`,
+                c3: `2 0`,
+            }
+        }
 
         let tie = true;
 
@@ -66,7 +94,8 @@ function Game() {
         }
 
         if (tie)
-            return 'tie';
+            return {
+        tie:'tie'};
 
         return -1; // still ongoing game
        }
@@ -74,8 +103,31 @@ function Game() {
 
        function playRound(symbol, cell)
        {
+        // First we need to check if the cell is already full
+        if (gameBoard[cell.r][cell.c]!= 'X' && gameBoard[cell.r][cell.c] != 'O' && gameBoard[cell.r][cell] != 'S')
+        {
         let {r,c } = cell;   
         gameBoard[r][c] = symbol;
+        return true
+        }
+       }
+
+
+       function checkForGameWinner(gameWinner)
+       {
+        gameOver = false;
+        if (gameWinner == 'X'){
+            console.log(`Player ${players[0]} Won!!`);
+            gameOver = true;}
+        else if (gameWinner == 'O'){
+            console.log(`Player ${players[1]} Won!!`);
+            gameOver = true;}
+
+        else if (gameWinner == 'tie'){
+        console.log("It's a tie!");
+        gameOver = true;}
+
+        return gameOver;
        }
 
        let gameTurn = true; 
@@ -98,21 +150,26 @@ function Game() {
           
     
                 let currentTurn;
+
+
+                
                 if (gameTurn == true)
                     currentTurn = 'X';
                 else
                     currentTurn = 'O';
     
     
-                    playRound(currentTurn, {r,c});
-    
-                    updateBoard({r,c}, currentTurn, event.target);
-        
+                    if(playRound(currentTurn, {r,c}) == true)
+                        {gameTurn = !gameTurn;
+                        updateBoard({r,c}, currentTurn, event.target);
+                        }
+            
                     let gameWinner;
     
     
-    
-                isWin = checkWinner()
+                winnerObject = checkWinner();
+                isWin = winnerObject.winner;
+                console.log(winnerObject);
     
                 if (isWin == 'X')
                 {
@@ -128,21 +185,40 @@ function Game() {
                     gameWinner = 'tie';
                 }
     
-                gameTurn = !gameTurn;
+                // gameTurn = !gameTurn;
     
     
             let gameOver = false;
-    
-            if (gameWinner == 'X'){
-                console.log(`Player ${players[0]} Won!!`);
-                gameOver = true;}
-            else if (gameWinner == 'O'){
-                console.log(`Player ${players[1]} Won!!`);
-                gameOver = true;}
-    
-            else if (gameWinner == 'tie'){
-            console.log("It's a tie!");
-            gameOver = true;}
+            
+            if (isWin == 'X' || isWin == 'O') // Game over
+            {
+                gameOver = checkForGameWinner(gameWinner);
+                let c1 = winnerObject.c1;
+                let c2 = winnerObject.c2;
+                let c3 = winnerObject.c3;
+                console.log(c1);
+
+                let cell1 = document.getElementById(c1);
+                let cell2 = document.getElementById(c2);
+                let cell3 = document.getElementById(c3);
+                
+                cell1.style.backgroundColor = '#22c55e';
+                cell2.style.backgroundColor = '#22c55e';
+                cell3.style.backgroundColor = '#22c55e';
+            }
+
+            else if (winnerObject.tie == 'tie')
+            {
+                console.log("It's a tie");
+                gameOver = true;
+
+                let cells = document.querySelectorAll('.box-cell');
+                cells.forEach(cell => {
+                    cell.style.backgroundColor = '#f8717144'
+                });
+            }
+
+            
     
     
             if (gameOver){
@@ -165,9 +241,6 @@ function Game() {
 let restartButton = document.querySelector('.restart');
 
 restartButton.addEventListener('click', startGame);
-
-
-
 let startGameEvent = new Event('click');
 
 
